da98e6e9d6b900b2b59af9f9c1ed65b2280eb8a8
Jianyu Zuo <zuowings@gmail.com>
2017-09-20 21:51:59 -0500

    Support MySQL
    MySQL database version for this website. Benefits:
    1. MySQL setups are useful
    2. MySQL has many good GUIs, for example [Sequel Pro],
    which gives better view and alternative managing on database data
    
    An explanation on how to install MySQL locally is attached.

--- yulp_app/app/controllers/static_pages_controller.rb
+++ yulp_app/app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ require 'yelp_client.rb'
 
 class StaticPagesController < ApplicationController
   def home
-
+    food_result_for_college_station = YelpClient.search({})
+    render json:food_result_for_college_station
   end
 
   def help
