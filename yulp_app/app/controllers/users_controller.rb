6a3b94bcd6c63361f9e79c05c4b09cec91a097be
Jianyu Zuo <zuowings@gmail.com>
2017-09-30 18:52:30 -0500

    Add generated files to Git
    These files are generated by the following command:
    RAILS_ENV=production bundle exec rake assets:precompile
    which are used for Heroku deployment process.

--- yulp_app/app/controllers/users_controller.rb
+++ yulp_app/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    byebug
+    # byebug
   end
 
   def create
---
+++ yulp_app/public/assets/users/show-8249be27153f6db72a27585884c2287dd4e8a989a9c1d0314237b672416bfd07.js
@@ -0,0 +1,1 @@
+(function(){$(function(){return alert("Pipeline Js Applied")})}).call(this);
