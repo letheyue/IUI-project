f901809b25fdcff6e34ab44a30f85ab6354dba3c
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-10-27 22:45:10 -0500

    Aggregated Commit for Rest View
    upload user_preferences again
    fix dropdown
    working on restaurant view

--- yulp_app/app/controllers/static_pages_controller.rb
+++ yulp_app/app/controllers/static_pages_controller.rb
@@ -3,6 +3,8 @@ require 'crawler_client.rb'
 
 
 class StaticPagesController < ApplicationController
+
+
   def home
     # food_result_for_college_station = YelpClient.search({})
     # render json:food_result_for_college_station
