6e5a1958a6dae30355a66bc32e68f73a0d8942bf
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-10-27 22:45:10 -0500

    just commit for pulling

--- yulp_app/app/assets/javascripts/application.js
+++ yulp_app/app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require bootstrap
 //= require Chart.bundle
 //= require chartkick
+//= require_tree .
