a2f842bba8536bfdd3096ea75e4735854bceb6fb
Tsao-Yuan Chang <joeychang0204@gmail.com>
2017-10-27 22:45:10 -0500

    Aggregated Commit for Rest View
    upload user_preferences again
    fix dropdown
    working on restaurant view

--- yulp_app/app/controllers/restaurants_controller.rb
+++ yulp_app/app/controllers/restaurants_controller.rb
@@ -4,10 +4,14 @@ class RestaurantsController < ApplicationController
 
   end
 
+<<<<<<< HEAD
   def index
     @restaurant = Restaurant.paginate(:page => params[:page],:per_page => 5).order(id: :asc).all
 
   end
+=======
+
+>>>>>>> Aggregated Commit for Rest View
 
 
 
