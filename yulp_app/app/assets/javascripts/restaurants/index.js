24dde7b789e484a569a1af71bf15555372ead6a9
JubenGithub <jency267@gmail.com>
2017-11-27 13:10:07 -0600

    minor adjustment

--- yulp_app/app/assets/javascripts/restaurants/index.js
+++ yulp_app/app/assets/javascripts/restaurants/index.js
@@ -78,3 +78,14 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For IE and Firefox
 }
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
